51f41837308eef42ddfe94853ef83faa8c7bfc25
tahsinrahman <tahsynx@gmail.com>
2018-03-13 18:37:02 +0600

    aaa

--- hack/concourse/test_pr.sh
+++ hack/concourse/test_pr.sh
@@ -7,3 +7,5 @@ CURRENT_DIR=$(pwd)
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r pull-request $GOPATH/src/github.com/appscode/guard
 cd $GOPATH/src/github.com/appscode/guard
+sleep 120
+#echo "hello world"
