9b54446f5b12229807faffdde5bb78831be95bba
tahsinrahman <tahsynx@gmail.com>
2018-03-13 16:48:37 +0600

    a

---
+++ hack/concourse/test_pr.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x -e
+
+echo "squash"
+CURRENT_DIR=$(pwd)
+mkdir -p $GOPATH/src/github.com/appscode
+cp -r pull-request $GOPATH/src/github.com/appscode/guard
+cd $GOPATH/src/github.com/appscode/guard
+echo "testing commit"
+git rev-parse HEAD
+go test -v ./...
